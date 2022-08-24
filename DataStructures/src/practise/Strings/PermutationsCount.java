fefd9c4e0efb75a8175a60d1f0eee02f0ec274dc
fMurugi <fionamurugi99@gmail.com>
2022-08-24 08:04:20 +0000

    added permutations of a string

---
+++ DataStructures/src/practise/Strings/PermutationsCount.java
@@ -0,0 +1,20 @@
+package practise.Strings;
+
+public class PermutationsCount {
+    public static void main(String[] args) {
+        System.out.println(permutationsCount("","abc"));
+    }
+    public static  int permutationsCount(String p, String up){
+        if(up.isEmpty()){
+            return 1;
+        }
+        char ch = up.charAt(0);
+        int count  = 0;
+        for(int i =0;i<=p.length();i++){
+            String f = p.substring(0,i);
+            String s = p.substring(i,p.length());
+            count = count + permutationsCount(f+ch+s,up.substring(1));
+        }
+        return count;
+    }
+}

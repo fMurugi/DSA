7137af04b1779abb9f5ba409fc7359912e07f987
fMurugi <fionamurugi99@gmail.com>
2024-01-15 11:53:14 +0300

    add unique candies problem, working with sets union,intersection to get element that are unique in two sets"

--- src/main/java/org/example/CrackingTheInterview/setsProblem2.java
+++ src/main/java/org/example/CrackingTheInterview/setsProblem2.java
@@ -1,6 +1,41 @@
 package org.example.CrackingTheInterview;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class setsProblem2 {
 
-    
+    public static void main(String[] args) {
+        int [] A ={1,2,3,4};
+        int [] B ={3,3,3,7};
+        System.out.println(numberOfUniqueCandies(A,B));
+
+    }
+
+    public static int numberOfUniqueCandies(int[]A,int[]B){
+        Set<Integer> setA = new HashSet<>();
+        Set<Integer> setB = new HashSet<>();
+        for(int a: A){
+            setA.add(a);
+        }
+
+
+        for(int b:B){
+            setB.add(b);
+        }
+        //initialize another set with the setA
+        Set<Integer> commonElements =  new HashSet<>(setA);
+        //intersection of setA nad setB
+        commonElements.retainAll(setB);
+
+        //union
+        setA.addAll(setB);
+
+        //union-intersection = uniqueElements
+        setA.removeAll(commonElements);
+        System.out.println(setA);
+        return setA.size();
+    }
 }
